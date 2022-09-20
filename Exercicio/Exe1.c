27fb0c312abdc8c17c04d242747a90fdc946d84f
Gabriel Piccolo <gabrielaltimari@hotmail.com>
2022-09-20 12:11:33 -0300

    Create Exe1.c

---
+++ Exercicio/Exe1.c
@@ -0,0 +1,15 @@
+#include<std.h>
+#include<stdlib.h>
+
+int main()
+{
+    float temp_C , temp_F;
+
+    scanf("%f",&temp_C);
+
+    temp_F= (9/5)*temp_C+32;
+
+    printf("%f, temp_F")
+    
+    return 0;
+}
